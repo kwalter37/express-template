1d4f3b0cb2275bfaafd627dd872765fcd2de30c3
Kevin Walter <kwalter@Kevins-MacBook-Pro.local>
2017-06-12 14:19:53 -0700

    initial version

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect;
+//var assert = require('assert'); //if you would rather use node's assert
+
+describe('UI', function() {
+  describe('Basic tests', function() {
+    it('Replace with real tests', function() {
+    	expect('Hello World!').to.equal('Hello World!');
+      //assert.equal('Hello World!', $('#hello').text());
+    });
+  });
+});
